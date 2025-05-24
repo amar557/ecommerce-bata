a12e937104bf55ceabaab2eeef192fda0d1b7ea1
amar ali <139375105+amar557@users.noreply.github.com>
2025-05-24 16:31:44 +0500

    links problem

--- server/index.js
+++ server/index.js
@@ -10,10 +10,11 @@ const app = express();
 
 app.use(express.json());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://ecommerce-bata.vercel.app"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
+
 
 app.use(cookieParser());
 app.use(cors(corsOptions));
